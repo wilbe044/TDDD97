5f2b85e280344aae2751a3144a5f76a01d4d116c
wille <wilbe044@student.liu.se>
2015-06-04 14:17:59 +0200

    selenium testing klart

--- Twidder/selenium_test.py
+++ Twidder/selenium_test.py
@@ -1,31 +1,42 @@
-from selenium.common.exceptions import NoSuchElementException, NoAlertPresentException
-
 __author__ = 'wille'
+
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-import unittest
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest, time, re
 
-def setUp(self):
-    self.driver = webdriver.Firefox()
-    self.driver.implicitly_wait(30)
-    self.base_url = "http://127.0.0.1:5000/"
-    self.verificationErrors = []
-    self.accept_next_alert = True
-
-class PythonTest(unittest.TestCase):
-
+class SeleniumTest(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(30)
+        self.base_url = "http://127.0.0.1:5000/"
+        self.verificationErrors = []
+        self.accept_next_alert = True
 
-
-    def selenium_test_py(self):
+    def test_selenium(self):
         driver = self.driver
         driver.get(self.base_url + "/")
-        driver.find_element_by_id("logInEmail").clear()
-        driver.find_element_by_id("logInEmail").send_keys("selenium@mail.com")
-        driver.find_element_by_id("logInPassword").clear()
-        driver.find_element_by_id("logInPassword").send_keys("password")
-        driver.find_element_by_id("logInSubmit").click()
+        driver.find_element_by_name("email").clear()
+        driver.find_element_by_name("email").send_keys("test@gmail.com")
+        driver.find_element_by_name("password").clear()
+        driver.find_element_by_name("password").send_keys("newpassword")
+        driver.find_element_by_css_selector("input.submitButton").click()
+        driver.find_element_by_id("message").clear()
+        driver.find_element_by_id("message").send_keys("test message to me")
+        driver.find_element_by_id("postButton").click()
+        driver.find_element_by_xpath("//button[@onclick='showDiv(2)']").click()
+        driver.find_element_by_id("otherUserEmail").clear()
+        driver.find_element_by_id("otherUserEmail").send_keys("gurka@gmail.com")
+        driver.find_element_by_css_selector("#searchUserForm > input[type=\"submit\"]").click()
+        driver.find_element_by_id("otherMessage").clear()
+        driver.find_element_by_id("otherMessage").send_keys("test message to other user")
+        driver.find_element_by_id("otherPostButton").click()
+        driver.find_element_by_css_selector("button").click()
+        driver.find_element_by_xpath("//button[@onclick='showDiv(3)']").click()
+        driver.find_element_by_xpath("//input[@value='signOut']").click()
 
     def is_element_present(self, how, what):
         try: self.driver.find_element(by=how, value=what)
@@ -33,13 +44,13 @@ class PythonTest(unittest.TestCase):
         return True
 
     def is_alert_present(self):
-        try: self.driver.switch_to.alert()
+        try: self.driver.switch_to_alert()
         except NoAlertPresentException, e: return False
         return True
 
     def close_alert_and_get_its_text(self):
         try:
-            alert = self.driver.switch_to.alert()
+            alert = self.driver.switch_to_alert()
             alert_text = alert.text
             if self.accept_next_alert:
                 alert.accept()
@@ -48,10 +59,9 @@ class PythonTest(unittest.TestCase):
             return alert_text
         finally: self.accept_next_alert = True
 
-
-
     def tearDown(self):
-        self.driver.close()
+        self.driver.quit()
+        self.assertEqual([], self.verificationErrors)
 
 if __name__ == "__main__":
     unittest.main()
